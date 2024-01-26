8cee61aedc9b3fb90a402eb1f26434799d7c09c1
Сашуся <olexandr.zhydenko@gmail.com>
2024-01-26 21:58:37 +0100

    New files

---
+++ blog/bloggernext.js
@@ -0,0 +1,11 @@
+﻿if (typeof(next_memory) == "undefined") next_memory = 0;
+{
+   var spans = document.getElementsByTagName("span");
+   var number = 0;
+   for(var i=0; i < spans.length; i++){
+      var c = " " + spans[i].className + " ";
+      if (c.indexOf("fullpost") != -1)number++;
+   }
+   if(number != next_memory)document.write("</span><span>");
+   next_memory = number;
+}
